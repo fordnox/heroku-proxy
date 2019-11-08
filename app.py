a82df084f53bfe4badfd38a816d9ccf2af9efea9
fordnox <fordnox@gmail.com>
2019-11-08 09:08:25 +0200

    Initial commit

--- app.py
+++ app.py
@@ -10,18 +10,26 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def root():
-    
-    url = request.args.get('url', '')
-    usage = 'Pass a properly encoded url parameter e.g. /?url=https://www.google.com'
+def home():
+    usage = 'Pass a properly encoded url parameter e.g. /https/www.google.com'
+    return usage
 
-    if url:
-        r = requests.get(url)
-        rr = Response(response=r.content, status=r.status_code)
-        rr.headers["Content-Type"] = r.headers['Content-Type']
-        return rr
-    else:
-        return usage
+@app.route('/https/<url>')
+def root(url):    
+    url = 'https://' + url
+    r = requests.get(url)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
+
+@app.route('/g/<keyword>')
+def gkeyword(keyword):    
+    url = 'https://www.google.com/search'
+    payload = {'q':keyword, 'num':1, 'start':1, 'sourceid':'chrome', 'ie':'UTF-8', 'cr':'cr=countryUS'}
+    r = requests.get(url, params=payload)
+    rr = Response(response=r.content, status=r.status_code)
+    rr.headers["Content-Type"] = r.headers['Content-Type']
+    return rr
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
